dad85c90d1be19686777c2eea80ebcb8437e55ae
charisTheo <harrytheo@google.com>
2024-08-16 18:17:04 +0100

    Added basic save button and snackbar notification.

--- lux-mea-lex/src/App.jsx
+++ lux-mea-lex/src/App.jsx
@@ -1,13 +1,28 @@
 import 'mdui/mdui.css';
+import 'mdui/components/button.js';
 import 'mdui/components/layout-main.js';
+import '@mdui/icons/save--outlined.js';
 
 import React from 'react';
+import { snackbar } from 'mdui/functions/snackbar.js';
 
 function App() {
 
+  const onSaveClick = () => {
+    const snackbarInstance = snackbar({
+      message: 'Saved!',
+      placement: 'top',
+      closeable: true,
+    });
+    snackbarInstance.classList.add('success');
+  };
+
   return (
     <mdui-layout-main>
-
+      <mdui-button variant="filled" onClick={onSaveClick}>
+        Save
+        <mdui-icon-save--outlined slot="icon" />
+      </mdui-button>
     </mdui-layout-main>
   );
 }
